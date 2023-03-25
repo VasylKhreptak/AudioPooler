91d0efd7f155a9622232fb66b25ee2a131bef449
VasylKhreptak <vasylkhreptak12@gmail.com>
2023-03-25 10:16:35 +0200

    Added Input Data Validation;

--- Assets/Plugins/AudioPooler/AudioPooler.cs
+++ Assets/Plugins/AudioPooler/AudioPooler.cs
@@ -4,6 +4,53 @@ namespace Plugins.AudioPooler
 {
     public class AudioPooler : MonoBehaviour
     {
+        [Header("References")]
+        [SerializeField] private Transform _transform;
 
+        [Header("Preferences")]
+        [SerializeField] private AudioData _defaultSettings;
+
+        [Header("Pool Preference")]
+        [SerializeField] private int _initialSize;
+        [SerializeField] private bool _autoExpand;
+        [SerializeField] private int _maxSize;
+
+        #region MonoBehaviour
+
+        private void OnValidate()
+        {
+            _transform ??= GetComponent<Transform>();
+
+            ValidateInputData();
+        }
+
+        #endregion
+
+        #region DataValidation
+
+        private void ValidateInputData()
+        {
+            ValidateInitialSize();
+
+            ValidateMaxSize();
+        }
+
+        private void ValidateInitialSize()
+        {
+            if (_initialSize < 1)
+            {
+                _initialSize = 1;
+            }
+        }
+
+        private void ValidateMaxSize()
+        {
+            if (_maxSize < _initialSize)
+            {
+                _maxSize = _initialSize;
+            }
+        }
+
+        #endregion
     }
 }
